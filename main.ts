73af68b02964ad2aa11bd0792167e45cd84d082a
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-15 13:18:42 -0400

    Task 1 random

--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
-let randomNumber = randint(0, 10)
-game.splash("Random number is" + randomNumber)
+let randomNumber = randint(1, 100)
+game.splash("Random number is " + randomNumber)
+let anotherRandom = randint(20, 30)
+game.splash("Second number is  " + anotherRandom)
